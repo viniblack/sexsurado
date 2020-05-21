d9dcf675f560eaadd91c67b05e3b220c9f78c788
Vini Black <viniibl4ck@gmail.com>
2020-05-20 22:45:13 -0300

    fiz umas alterações que nem me lembro mais o que foi..

--- js/index.js
+++ js/index.js
@@ -1,30 +1,39 @@
-$("#mover").mousedown(function() {
+$("#mover").mousedown(function () {
     $("#allmenu").draggable();
-    $("#allmenu").draggable( "enable" );
-    $("#allmenu").draggable({ containment: 'window'});
+    $("#allmenu").draggable("enable");
+    $("#allmenu").draggable({
+        containment: 'window'
+    });
 })
 
-$("#mover").mouseup(function() {
+$("#mover").mouseup(function () {
     $("#allmenu").draggable();
-    $("#allmenu").draggable( "disable" );
+    $("#allmenu").draggable("disable");
 })
 
-$(".reset").click(function() {
+$(".reset").click(function () {
     $("#draggable").css({
         'left': $("#draggable").data('originalLeft'),
         'top': $("#draggable").data('origionalTop')
     });
 });
 
-
 $("#allmenu").data({
     'originalLeft': $("#allmenu").css('left'),
     'origionalTop': $("#allmenu").css('top')
 });
 
-$(".reset").click(function() {
+$(".reset").click(function () {
     $("#allmenu").css({
         'left': $("#allmenu").data('originalLeft'),
         'top': $("#allmenu").data('origionalTop')
     });
 });
+
+let tamanhoTela = $(document).width();
+
+console.log(tamanhoTela);
+
+if(tamanhoTela < 1400) {
+    $(this).toggleClass('col-md-6')
+}
