84815e20bd9d587afe1f573934a7921f8ac9b4e9
Vini Black <51183682+viniblack@users.noreply.github.com>
2020-07-23 16:05:28 -0300

    criando a janela para a radio, pronto para apresentar na banca

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$("#mover").mousedown(function () {
+$("#mover").mousedown(function() {
     $("#allmenu").draggable();
     $("#allmenu").draggable("enable");
     $("#allmenu").draggable({
@@ -6,12 +6,12 @@ $("#mover").mousedown(function () {
     });
 })
 
-$("#mover").mouseup(function () {
+$("#mover").mouseup(function() {
     $("#allmenu").draggable();
     $("#allmenu").draggable("disable");
 })
 
-$(".reset").click(function () {
+$(".reset").click(function() {
     $("#draggable").css({
         'left': $("#draggable").data('originalLeft'),
         'top': $("#draggable").data('origionalTop')
@@ -23,15 +23,53 @@ $("#allmenu").data({
     'origionalTop': $("#allmenu").css('top')
 });
 
-$(".reset").click(function () {
+$(".reset").click(function() {
     $("#allmenu").css({
         'left': $("#allmenu").data('originalLeft'),
         'top': $("#allmenu").data('origionalTop')
     });
 });
 
-$('.card-deck a').fancybox({
-    caption: function (instance, item) {
-        return $(this).parent().find('.card-text').html();
-    }
+
+
+
+
+$("#mover1").mousedown(function() {
+    $("#allmenu1").draggable();
+    $("#allmenu1").draggable("enable");
+    $("#allmenu1").draggable({
+        containment: 'window'
+    });
+})
+
+$("#mover1").mouseup(function() {
+    $("#allmenu1").draggable();
+    $("#allmenu1").draggable("disable");
+})
+
+$(".reset1").click(function() {
+    $("#draggable").css({
+        'left': $("#draggable").data('originalLeft'),
+        'top': $("#draggable").data('origionalTop')
+    });
 });
+
+$("#allmenu1").data({
+    'originalLeft': $("#allmenu1").css('left'),
+    'origionalTop': $("#allmenu1").css('top')
+});
+
+$(".reset1").click(function() {
+    $("#allmenu1").css({
+        'left': $("#allmenu1").data('originalLeft'),
+        'top': $("#allmenu1").data('origionalTop')
+    });
+});
+
+function setTopo() {
+    $(window).scrollTop(0);
+}
+$(window).bind('scroll', setTopo);
+
+let aud = document.getElementById("radio");
+aud.volume = 0.01;
